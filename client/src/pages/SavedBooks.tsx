7cf0a21cdc3944482ea288e7e81fda8d4309446d
David <dumana92@gmail.com>
2024-12-09 20:07:04 -0500

    Commented for build to complete

--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -1,63 +1,63 @@
-import { useQuery, useMutation } from "@apollo/client";
-import { Container, Card, Button, Row, Col } from "react-bootstrap";
+// import { useQuery, useMutation } from "@apollo/client";
+// import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
-import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations";
-import Auth from "../utils/auth";
-import { removeBookId } from "../utils/localStorage";
-import type { Book } from "../models/Book";
+// import { GET_ME } from "../utils/queries";
+// import { REMOVE_BOOK } from "../utils/mutations";
+// import Auth from "../utils/auth";
+// import { removeBookId } from "../utils/localStorage";
+// import type { Book } from "../models/Book";
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || { SavedBooks: [] };
+  // const { loading, data } = useQuery(GET_ME);
+  // const userData = data?.me || { SavedBooks: [] };
 
-  const [deleteBook] = useMutation(REMOVE_BOOK, {
-    update(cache, { data: { deleteBook } }) {
-      if (deleteBook) {
-        cache.modify({
-          fields: {
-            me(existingMeData = {}) {
-              return {
-                ...existingMeData,
-                savedBooks: existingMeData.savedBooks.filter(
-                  (book: Book) => book.bookId !== deleteBook.bookId
-                ),
-              };
-            },
-          },
-        });
-        removeBookId(deleteBook.bookId);
-      }
-    },
-  });
+  // const [deleteBook] = useMutation(REMOVE_BOOK, {
+  //   update(cache, { data: { deleteBook } }) {
+  //     if (deleteBook) {
+  //       cache.modify({
+  //         fields: {
+  //           me(existingMeData = {}) {
+  //             return {
+  //               ...existingMeData,
+  //               savedBooks: existingMeData.savedBooks.filter(
+  //                 (book: Book) => book.bookId !== deleteBook.bookId
+  //               ),
+  //             };
+  //           },
+  //         },
+  //       });
+  //       removeBookId(deleteBook.bookId);
+  //     }
+  //   },
+  // });
 
-  const handleDeleteBook = async (bookId: string) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-    if (!token) {
-      return false;
-    }
+  // const handleDeleteBook = async (bookId: string) => {
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+  //   if (!token) {
+  //     return false;
+  //   }
 
-    try {
-      const { data } = await deleteBook({
-        variables: { bookId },
-      });
+  //   try {
+  //     const { data } = await deleteBook({
+  //       variables: { bookId },
+  //     });
 
-      if (data && data.removeBook) {
-        console.log("Book deleted successfully!");
-        removeBookId(bookId);
-      }
-    } catch (err) {
-      console.error("Error deleting book:", err);
-    }
-  };
+  //     if (data && data.removeBook) {
+  //       console.log("Book deleted successfully!");
+  //       removeBookId(bookId);
+  //     }
+  //   } catch (err) {
+  //     console.error("Error deleting book:", err);
+  //   }
+  // };
 
-  if (loading) {
-    return <h2>LOADING...</h2>;
-  }
+  // if (loading) {
+  //   return <h2>LOADING...</h2>;
+  // }
 
   return (
     <>
-      <div className="text-light bg-dark p-5">
+      {/* <div className="text-light bg-dark p-5">
         <Container>
           {userData.username ? (
             <h1>Viewing {userData.username}'s saved books!</h1>
@@ -102,7 +102,7 @@ const SavedBooks = () => {
             );
           })}
         </Row>
-      </Container>
+      </Container> */}
     </>
   );
 };
