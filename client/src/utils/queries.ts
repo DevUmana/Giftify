2939b7fbe794c641f2a746719bec1e1cfb2b36f8
DOni5503 <dboyer5503@gmail.com>
2024-12-09 19:08:43 -0500

    removed comments in queries.ts

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -17,21 +17,3 @@ export const GET_ME = gql`
     }
   }
 `;
-
-// export interface IUserDocument extends Document {
-//  id: string;
- // username: string;
- // email: string;
- // password: string;
- // recipientList: RecipientDocument[];
- // isCorrectPassword(password: string): Promise<boolean>;
- // recipientCount: number;
-//}
-
-// export interface RecipientDocument extends Document {
-// name: string;
-// gifts: string[];
-// recipientId: string;
-// budget: number;
-// status: boolean;
-//}
